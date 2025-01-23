fe419949519ca4a95be2b640c9a2145ec5160844
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2025-01-23 23:56:37 +0800

    improve skeletal version of Ozymandias to echo the commands by the user and exits when the user types the command bye

--- src/main/java/Ozymandias.java
+++ src/main/java/Ozymandias.java
@@ -1,16 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Ozymandias {
     public static String greetHello() {
-        return "____________________________________________________________ \n" +
-                "Hello! I'm Ozymandias! \nWhat can I do for you?"
-                + "\n____________________________________________________________ ";
+        return "    ____________________________________________________________ \n" +
+                "    Hello! I'm Ozymandias\n    What can I do for you?"
+                + "\n    ____________________________________________________________ ";
     }
 
     public static String greetGoodbye() {
-        return "Bye. Hope to see you again soon!"
-                + "\n____________________________________________________________";
+        return "    Bye. Hope to see you again soon!"
+                + "\n    ____________________________________________________________";
     }
     public static void main(String[] args) {
         System.out.println(greetHello());
-        System.out.println(greetGoodbye());
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(greetGoodbye());
+                return;
+            }
+            System.out.println("    ____________________________________________________________ \n" +
+                    "    " + input
+                    + "\n    ____________________________________________________________ ");
+        }
     }
 }
