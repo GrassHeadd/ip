c7b15063558ed212ecda8c12a1f3fccc3daf89ce
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2025-02-20 13:23:28 +0800

    edit toggle

--- src/main/java/Ozymandias/Tasks/Task.java
+++ src/main/java/Ozymandias/Tasks/Task.java
@@ -13,15 +13,6 @@ public abstract class Task {
 
     public void toggleIsDone() {
         isDone = !isDone;
-        if (isDone) {
-            System.out.println("    Nice! I've marked this task as done:");
-            System.out.println("      [X] " + taskDetails);
-            System.out.println();
-        } else {
-            System.out.println("    OK, I've marked this task as not done yet:");
-            System.out.println("      [ ] " + taskDetails);
-            System.out.println();
-        }
     }
 
     public String getStatusIcon() {
