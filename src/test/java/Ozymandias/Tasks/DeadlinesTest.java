8afb6bc1bc7fa8748d3064b6eae76c3b6683819d
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2025-02-20 19:01:36 +0800

    add ui

--- src/test/java/Ozymandias/Tasks/DeadlinesTest.java
+++ src/test/java/Ozymandias/Tasks/DeadlinesTest.java
@@ -1,18 +1,19 @@
 package Ozymandias.Tasks;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeadlinesTest {
 
     @Test
     public void testDeadlineCreation() {
-        Deadlines deadline = new Deadlines("Submit Assignment", "2025-10-15");
-        assertEquals("[D]", deadline.getTaskType());
+        Deadlines deadlines = new Deadlines("Submit Assignment", "2025-10-15");
+        assertEquals("[D]", deadlines.getTaskType());
     }
 
     @Test
     public void testDeadlineToString() {
-        Deadlines deadline = new Deadlines("Submit Assignment", "2025-10-15");
-        assertEquals("Submit Assignment (by: Oct 15 2025)", deadline.toString());
+        Deadlines deadlines = new Deadlines("Submit Assignment", "2025-10-15");
+        assertEquals("Submit Assignment (by: Oct 15 2025)", deadlines.toString());
     }
 }
--- src/test/java/Ozymandias/Tasks/EventsTest.java
+++ src/test/java/Ozymandias/Tasks/EventsTest.java
@@ -2,6 +2,8 @@ package Ozymandias.Tasks;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EventsTest {
 
     @Test
